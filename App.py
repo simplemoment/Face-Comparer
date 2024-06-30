4413cf9485af077682a3daa62b208f04e6d00aa5
PYROX-PROJECTS <109022902+Eg0rAk28@users.noreply.github.com>
2024-06-30 14:55:20 +0700

    Update App.py

--- App.py
+++ App.py
@@ -4,12 +4,10 @@ import gradio as gr
 import math
 import numpy as np
 
-print("Launching...")
 def resource_path(path = "") -> str:
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, path)
     else:
-        print(os.path.join(os.path.abspath("."), path))
         return os.path.join(os.path.abspath("."), path)
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
@@ -17,15 +15,11 @@ from deepface import DeepFace
 
 window_desc = "This is service for compare, analyse and recognize faces!"
 window_tag = "DeepFace-CompareR"
-window_icon_path = "DF-C.ico"
 logging.basicConfig(level=logging.INFO)
 
 face2face_dropdown = ["VGG-Face", "Facenet", "DeepFace", "OpenFace"]
 face2face_checkboxes = ["cosine", "euclidean", "euclidean_l2"]
 mytheme = gr.themes.Soft()
-mytheme.set(
-    # body_background_fill="linear-gradient(0deg, dark, white)"
-)
 
 class DF:
     def __init__(self):
@@ -75,20 +69,12 @@ class DF:
     def face2face_compare(self, fp0, fp1, model0, metric0):
         print(fp0, fp1, model0, metric0[0])
         result_verify = DeepFace.verify(img1_path=fp0, img2_path=fp1, model_name=model0, distance_metric=metric0[0])
-        # result_final = self.plot_fixed_data((0, result_verify['distance']*100), ("N\\S", "Distance"))
-        # f_time = time.strftime("%y.%m.%d")
-        # s_time = time.strftime("%H~%M~%S")
-        # f_path = f'./diagram_{f_time}-{s_time}.png'
-        # result_final.imsave(f_path, result_final.get(plt))
 
-        print(str(result_verify))
-        # return gr.Text(value=str(result_verify))
 
         verify_status = 'Failed'
         if result_verify['verified']: verify_status = 'Success!'
 
         return gr.Text(value=f"Result: {verify_status}\n\nDistance is: {round(number=float(result_verify['distance']), ndigits=2)}\nThreshold used: {result_verify['threshold']}\nModel name: {result_verify['model']}\nSimilarity metric: {result_verify['similarity_metric']}\n\nTime, used for this operation: {result_verify['time']} seconds")
-        # return DeepFace.analyze()
 
 
     def face2fld_compare(self, fp0, fp1, model0, metric0, trhold0):
